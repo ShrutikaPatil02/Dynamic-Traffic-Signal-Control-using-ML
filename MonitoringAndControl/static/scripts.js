97928d3c888b610f3d87a653ee4302183510202b
MasterQuill <zaibgp10@gmail.com>
2024-10-17 09:00:12 +0530

    model is working on images for now. timer functionality has also been added

--- MonitoringAndControl/static/scripts.js
+++ MonitoringAndControl/static/scripts.js
@@ -2,8 +2,15 @@ function updateData(){
     fetch('http://127.0.0.1:8000/getData')
     .then(response => response.json())
     .then(data =>{
+        document.getElementById('noOfVehiclesNorth').innerHTML = data.noOfVehiclesNorth;
+        document.getElementById('timerNorth').innerHTML = data.timerNorth;
+        document.getElementById('noOfVehiclesEast').innerHTML = data.noOfVehiclesEast;
+        document.getElementById('timerEast').innerHTML = data.timerEast;
         document.getElementById('noOfVehiclesWest').innerHTML = data.noOfVehiclesWest;
+        document.getElementById('timerWest').innerHTML = data.timerWest;
+        document.getElementById('noOfVehiclesSouth').innerHTML = data.noOfVehiclesSouth;
+        document.getElementById('timerSouth').innerHTML = data.timerSouth;
     });
 }
 
-setInterval(updateData,1000);
+setInterval(updateData,100);
--- MonitoringAndControl/views.py
+++ MonitoringAndControl/views.py
@@ -3,18 +3,25 @@ from django.http import HttpResponse
 from django.template import loader
 from django.http import JsonResponse
 import cv2 as cv
-import os
 import numpy as np
 from ultralytics import YOLO
+import datetime
 
 model = YOLO("best.pt")
 
-cam = cv.VideoCapture(0)
+#cam = cv.VideoCapture(0)
+token = 1
+
+timeTemp = datetime.datetime.now() + datetime.timedelta(0,2,0)
 
 data = {'noOfVehiclesWest':0,
+        'timerWest':0,
         'noOfVehiclesNorth':0,
+        'timerNorth':0,
         'noOfVehiclesSouth':0,
-        'noOfVehiclesEast':0,        
+        'timerSouth':0,
+        'noOfVehiclesEast':0,   
+        'timerEast':0,     
 }
 
 def homepage(request):
@@ -29,19 +36,86 @@ def homepage(request):
 
 
 def getData(request):
-    ret,frame = cam.read()
-    model = YOLO("best.pt")
-    results = model(frame)
+    global token,timeTemp
+    if(token == 1 and data['timerWest'] == 0):
+        token = 2
+        frame = cv.imread('trafficTest2.jpeg')
+        results = model(frame)
 
-    for r in results:
-        numberOfVehicles = 0
-        boxTensor = r.boxes.xyxy
-        boxArray = boxTensor.cpu().numpy()
-        for i in boxArray:
-            numberOfVehicles += 1
+        for r in results:
+            numberOfVehicles = 0
+            boxTensor = r.boxes.xyxy
+            boxArray = boxTensor.cpu().numpy()
+            for i in boxArray:
+                numberOfVehicles += 1
 
-    data['noOfVehiclesWest'] = numberOfVehicles
-    data['noOfVehiclesNorth'] = numberOfVehicles
-    data['noOfVehiclesSouth'] = numberOfVehicles
-    data['noOfVehiclesEast'] = numberOfVehicles
+        data['noOfVehiclesNorth'] = numberOfVehicles
+        timeTemp += datetime.timedelta(0,numberOfVehicles,0)
+        difference = timeTemp - datetime.datetime.now()
+        data['timerNorth'] = difference.total_seconds()
+        return JsonResponse(data)
+    elif(token ==2 and data['timerNorth']!=0):
+        difference = timeTemp - datetime.datetime.now()
+        data['timerNorth'] = difference.seconds
+        return JsonResponse(data) 
+    elif(token == 2 and data['timerNorth'] == 0):
+        token = 3
+        frame = cv.imread('trafficTest3.jpeg')
+        results = model(frame)
+
+        for r in results:
+            numberOfVehicles = 0
+            boxTensor = r.boxes.xyxy
+            boxArray = boxTensor.cpu().numpy()
+            for i in boxArray:
+                numberOfVehicles += 1
+        data['noOfVehiclesEast'] = numberOfVehicles
+        timeTemp += datetime.timedelta(0,numberOfVehicles,0)
+        difference = timeTemp - datetime.datetime.now()
+        data['timerEast'] = difference.total_seconds()
+        return JsonResponse(data)
+    elif(token == 3 and data['timerEast']!=0):
+        difference = timeTemp - datetime.datetime.now()
+        data['timerEast'] = difference.seconds
+        return JsonResponse(data)
+    elif(token == 3 and data['timerEast']==0):
+        token = 4
+        frame = cv.imread('trafficTest4.jpeg')
+        results = model(frame)
+
+        for r in results:
+            numberOfVehicles = 0
+            boxTensor = r.boxes.xyxy
+            boxArray = boxTensor.cpu().numpy()
+            for i in boxArray:
+                numberOfVehicles += 1
+        data['noOfVehiclesSouth'] = numberOfVehicles
+        timeTemp += datetime.timedelta(0,numberOfVehicles,0)
+        difference = timeTemp - datetime.datetime.now()
+        data['timerSouth'] = difference.total_seconds()
+        return JsonResponse(data)
+    elif(token == 4 and data['timerSouth']!=0):
+        difference = timeTemp - datetime.datetime.now()
+        data['timerSouth'] = difference.seconds
+        return JsonResponse(data)
+    elif(token == 4 and data['timerSouth'] == 0):
+        token = 1
+        frame = cv.imread('trafficTest.jpeg')
+        results = model(frame)
+
+        for r in results:
+            numberOfVehicles = 0
+            boxTensor = r.boxes.xyxy
+            boxArray = boxTensor.cpu().numpy()
+            for i in boxArray:
+                numberOfVehicles += 1
+        data['noOfVehiclesWest'] = numberOfVehicles
+        timeTemp += datetime.timedelta(0,numberOfVehicles,0)
+        difference = timeTemp - datetime.datetime.now()
+        data['timerWest'] = difference.total_seconds()
+        return JsonResponse(data)
+    elif(token == 1 and data['timerWest']!=0):
+        difference = timeTemp - datetime.datetime.now()
+        data['timerWest'] = difference.seconds
+        return JsonResponse(data)
     return JsonResponse(data)
