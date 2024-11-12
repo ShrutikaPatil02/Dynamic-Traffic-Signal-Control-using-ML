1f8efe2e63ce9c682d4f7c8485f7498e1260f61c
ShrutikaPatil02 <shrutikapatil2102@gmail.com>
2024-11-12 17:43:43 +0530

    implemented multithreading

--- MonitoringAndControl/views.py
+++ MonitoringAndControl/views.py
@@ -7,13 +7,13 @@ import numpy as np
 from ultralytics import YOLO
 import datetime
 from django.http import StreamingHttpResponse
+import threading
+import time
 
 model = YOLO("best.pt")
 
 #cam = cv.VideoCapture(0)
 token = 1
-
-timeTemp = datetime.datetime.now() + datetime.timedelta(0,2,0)
 
 data = {'noOfVehiclesWest':0,
         'timerWest':0,
@@ -24,6 +24,84 @@ data = {'noOfVehiclesWest':0,
         'noOfVehiclesEast':0,   
         'timerEast':0,     
 }
+
+timeTemp = datetime.datetime.now() + datetime.timedelta(0,2,0)
+
+def detectVehicleSouth():
+    global model
+    frame = cv.imread('trafficTest4.jpeg')
+    results = model(frame)
+
+    
+    for r in results:
+        numberOfVehicles = 0
+        boxTensor = r.boxes.xyxy
+        boxArray = boxTensor.cpu().numpy()
+        for i in boxArray:
+            numberOfVehicles += 1
+    data['noOfVehiclesSouth'] = numberOfVehicles
+    time.sleep(5)
+    detectVehicleSouth()
+
+
+def detectVehicleNorth():
+    global model
+    
+    frame = cv.imread('trafficTest2.jpeg')
+    results = model(frame)
+    
+    for r in results:
+        numberOfVehicles = 0
+        boxTensor = r.boxes.xyxy
+        boxArray = boxTensor.cpu().numpy()
+        for i in boxArray:
+            numberOfVehicles += 1
+    data['noOfVehiclesNorth'] = numberOfVehicles
+    time.sleep(5)
+    detectVehicleNorth()
+
+
+
+def detectVehicleWest():
+    global model
+    
+    frame = cv.imread('trafficTest.jpeg')
+    results = model(frame)
+    for r in results:
+        numberOfVehicles = 0
+        boxTensor = r.boxes.xyxy
+        boxArray = boxTensor.cpu().numpy()
+        for i in boxArray:
+            numberOfVehicles += 1
+    data['noOfVehiclesWest'] = numberOfVehicles
+    time.sleep(5)
+    detectVehicleWest()
+
+def detectVehicleEast():
+    global model
+    
+    frame = cv.imread('trafficTest3.jpeg')
+    results = model(frame)
+    for r in results:
+        numberOfVehicles = 0
+        boxTensor = r.boxes.xyxy
+        boxArray = boxTensor.cpu().numpy()
+        for i in boxArray:
+            numberOfVehicles += 1
+    data['noOfVehiclesEast'] = numberOfVehicles
+    time.sleep(5)
+    detectVehicleEast()
+
+f5 = threading.Thread(target=detectVehicleWest)
+f2 = threading.Thread(target=detectVehicleEast)
+f3 = threading.Thread(target=detectVehicleSouth)
+f4 = threading.Thread(target=detectVehicleNorth)
+
+f5.start()
+f2.start()
+f3.start()
+f4.start()
+
 
 def homepage(request):
     template = loader.get_template('homepage.html')
@@ -40,20 +118,10 @@ def getData(request):
     global token,timeTemp
     if(token == 1 and data['timerWest'] == 0):
         token = 2
-        frame = cv.imread('trafficTest2.jpeg')
-        results = model(frame)
-
-        for r in results:
-            numberOfVehicles = 0
-            boxTensor = r.boxes.xyxy
-            boxArray = boxTensor.cpu().numpy()
-            for i in boxArray:
-                numberOfVehicles += 1
-
-        timeAlloted = numberOfVehicles*0.57+9.40
+        timeAlloted = data["noOfVehiclesNorth"]*0.57+9.40
         if(timeAlloted<15):
             timeAlloted = 15
-        data['noOfVehiclesNorth'] = numberOfVehicles
+        #data['noOfVehiclesNorth'] = numberOfVehicles
         timeTemp += datetime.timedelta(0,timeAlloted,0)
         difference = timeTemp - datetime.datetime.now()
         data['timerNorth'] = difference.total_seconds()
@@ -64,20 +132,9 @@ def getData(request):
         return JsonResponse(data) 
     elif(token == 2 and data['timerNorth'] == 0):
         token = 3
-        frame = cv.imread('trafficTest3.jpeg')
-        results = model(frame)
-
-        for r in results:
-            numberOfVehicles = 0
-            boxTensor = r.boxes.xyxy
-            boxArray = boxTensor.cpu().numpy()
-            for i in boxArray:
-                numberOfVehicles += 1
-
-        timeAlloted = numberOfVehicles*0.57+9.40
+        timeAlloted = data["noOfVehiclesEast"]*0.57+9.40
         if(timeAlloted<15):
             timeAlloted = 15
-        data['noOfVehiclesEast'] = numberOfVehicles
         timeTemp += datetime.timedelta(0,timeAlloted,0)
         difference = timeTemp - datetime.datetime.now()
         data['timerEast'] = difference.total_seconds()
@@ -88,20 +145,10 @@ def getData(request):
         return JsonResponse(data)
     elif(token == 3 and data['timerEast']==0):
         token = 4
-        frame = cv.imread('trafficTest4.jpeg')
-        results = model(frame)
-
-        for r in results:
-            numberOfVehicles = 0
-            boxTensor = r.boxes.xyxy
-            boxArray = boxTensor.cpu().numpy()
-            for i in boxArray:
-                numberOfVehicles += 1
-        
-        timeAlloted = numberOfVehicles*0.57+9.40
+        timeAlloted = data["noOfVehiclesSouth"]*0.57+9.40
         if(timeAlloted<15):
             timeAlloted = 15
-        data['noOfVehiclesSouth'] = numberOfVehicles
+        
         timeTemp += datetime.timedelta(0,timeAlloted,0)
         difference = timeTemp - datetime.datetime.now()
         data['timerSouth'] = difference.total_seconds()
@@ -112,20 +159,9 @@ def getData(request):
         return JsonResponse(data)
     elif(token == 4 and data['timerSouth'] == 0):
         token = 1
-        frame = cv.imread('trafficTest5.jpeg')
-        results = model(frame)
-
-        for r in results:
-            numberOfVehicles = 0
-            boxTensor = r.boxes.xyxy
-            boxArray = boxTensor.cpu().numpy()
-            for i in boxArray:
-                numberOfVehicles += 1
-
-        timeAlloted = numberOfVehicles*0.57+9.40
+        timeAlloted = data["noOfVehiclesWest"]*0.57+9.40
         if(timeAlloted<15):
             timeAlloted = 15
-        data['noOfVehiclesWest'] = numberOfVehicles
         timeTemp += datetime.timedelta(0,timeAlloted,0)
         difference = timeTemp - datetime.datetime.now()
         data['timerWest'] = difference.total_seconds()
@@ -151,3 +187,4 @@ def vidStream():
         
 def videoFeed(request):
     return StreamingHttpResponse(vidStream(),content_type = 'multipart/x-mixed-replace; boundary=frame')
+
